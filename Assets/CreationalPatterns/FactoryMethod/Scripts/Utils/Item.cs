836e68970028afe553f18f8775d2c034e3bd401a
NickPansh <nickpansh@yeah.net>
2023-02-20 17:21:44 +0800

    工厂方法的Item改成Abstract
    Item类不需要被初始化，将它改成抽象类以便于给Item添加抽象方法

--- Assets/CreationalPatterns/FactoryMethod/Scripts/Utils/Item.cs
+++ Assets/CreationalPatterns/FactoryMethod/Scripts/Utils/Item.cs
@@ -2,8 +2,8 @@
  * @Author: NickPansh
  * @Date: 2023-02-01 12:46:01
  * @LastEditors: NickPansh
- * @LastEditTime: 2023-02-01 13:42:03
- * @FilePath: \Unity-Design-Pattern\Assets\FactoryMethod\Scripts\Utils\Item.cs
+ * @LastEditTime: 2023-02-20 17:15:13
+ * @FilePath: \Unity-Design-Pattern\Assets\CreationalPatterns\FactoryMethod\Scripts\Utils\Item.cs
  * @Description: 
  * @
  * @Copyright (c) 2023 by nickpansh@yeah.net | wenqu.site, All Rights Reserved. 
@@ -11,13 +11,8 @@
 using UnityEngine;
 namespace WenQu.FactoryMethod
 {
-    public class Item : MonoBehaviour
+    public abstract class Item : MonoBehaviour
     {
         public string id;
-
-        public Item()
-        {
-
-        }
     }
 }
